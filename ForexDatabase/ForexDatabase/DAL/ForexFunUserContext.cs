dae38463dceb563a568f74ef16782c2d967f22c9
Bzdeco <jakugwiz@student.agh.edu.pl>
2018-06-16 15:28:39 +0200

    Registration using Identity Service

---
+++ ForexDatabase/ForexDatabase/DAL/ForexFunUserContext.cs
@@ -0,0 +1,28 @@
+﻿using PostgreSQL.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ForexDatabase.DAL
+{
+    public class ForexFunUserContext : IdentityDbContext<ForexFunUser>
+    {
+        public ForexFunUserContext() : base("DatabaseContext", false)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("public");
+            modelBuilder.Entity<ForexFunUser>().ToTable("User");
+            // TODO is necessary?
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
+        }
+    }
+}
---
+++ ForexDatabase/ForexDatabase/Models/ForexFunUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForexDatabase.Models
+{
+    public class ForexFunUserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
