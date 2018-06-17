66f7a24f5318a858959aea2f482854094607b06e
Łukasz Ściga <scigalukasz@gmail.com>
2018-06-17 23:24:06 +0200

    welp

---
+++ ForexDatabase/DatabaseAccessLayer/ForexFunUser.cs
@@ -0,0 +1,12 @@
+﻿using PostgreSQL.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForexDatabase.DAL
+{
+    public class ForexFunUser : IdentityUser
+    {
+    }
+}
