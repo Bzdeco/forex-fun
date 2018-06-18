90c8a050abe5736295d90ca68e4d77d772a26c2b
Łukasz Ściga <lick@student.agh.edu.pl>
2018-06-18 12:01:41 +0200

    test kafka version

--- ForexDatabase/RecordsPublisher/Program.cs
+++ ForexDatabase/RecordsPublisher/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
 using ForexDatabase.DAL;
+using Model;
 using Quartz;
 using Quartz.Impl;
 
@@ -54,32 +56,25 @@ namespace RecordsPublisher
 
     class PublisherJob : IJob
     {
-        private static readonly string USERS_TOPIC = "060yyody-users";
-        private static readonly string TRANSACTIONS_TOPIC = "060yyody-transactions";
-        private static readonly string JAAS_CONFIG = String.Format("org.apache.kafka.common.security.scram.ScramLoginModule required username=\"{0}\" password=\"{1}\";",
-            "060yyody", "QqZNAE-Z5paRREQqOrrnWIFB9WQv7LVC");
+        private static readonly string USERS_TOPIC = "user-log";
+        private static readonly string TRANSACTIONS_TOPIC = "transaction-log";
         private static readonly Dictionary<string, object> kafkaConfig = new Dictionary<string, object>
         {
-            { "bootstrap.servers", "ark-01.srvs.cloudkafka.com:9094,ark-02.srvs.cloudkafka.com:9094,ark-03.srvs.cloudkafka.com:9094" },
-            { "sasl.jaas.config", JAAS_CONFIG}
+            { "bootstrap.servers", "192.168.43.70:9092" }
         };
-
-        private DatabaseContext db = new DatabaseContext();
 
         public async Task Execute(IJobExecutionContext context)
         {
             Console.WriteLine("Task launched.");
-            Console.WriteLine("JAAS CONF: " + JAAS_CONFIG);
-            var usersCount = db.Users.Count();
-            var transactionsCount = db.Transactions.Count();
-            new Producer<Null, int>(kafkaConfig, null, new IntSerializer());
 
-            using (var producer = new Producer<Null, int>(kafkaConfig, null, new IntSerializer()))
+            string usersCount = "0";
+            string transactionsCount = "0";
+
+            using (var producer = new Producer<Null, string>(kafkaConfig, null, new StringSerializer(Encoding.UTF8)))
             {
                 Console.WriteLine("Attempting to write data: users - " + usersCount + ", transactions - " + transactionsCount);
                 var usersResult = producer.ProduceAsync(USERS_TOPIC, null, usersCount).Result;
                 var transactionsResult = producer.ProduceAsync(TRANSACTIONS_TOPIC, null, transactionsCount).Result;
-                Console.WriteLine("Got data: users - " + usersResult + ", transactions - " + transactionsResult);
                 await Console.Out.WriteLineAsync("Got data: users - " + usersResult + ", transactions - " + transactionsResult);
             }
         }
